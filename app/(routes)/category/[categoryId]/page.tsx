7a5c180cc40b9429ec2e9f9643d0de79f0c78892
unknown <facuugerez37@gmail.com>
2024-02-28 19:24:38 -0300

    filters and individually categories page done

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,9 @@ import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 import Filter from "./components/filter";
+import MobileFilter from "./components/mobile-filter";
+import NoResults from "@/components/ui/no-result";
+import ProductCard from "@/components/ui/product-card";
 
 export const revalidate = 0;
 
@@ -38,12 +41,32 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({ params, searchParams 
                 <div className="px-4 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                         {/* Mobile Filters*/}
+                        
+                        <MobileFilter sizes={sizes} colors={colors} />
+
+                        {/* Desktop Web Filters */}
                         <div className="hidden lg:block">
                             <Filter 
                                 valueKey="sizeId"
                                 name="Sizes"
                                 data={sizes}
                             />
+                             <Filter 
+                                valueKey="colorId"
+                                name="Colors"
+                                data={colors}
+                            />
+                        </div>
+                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                            {products.length === 0 && <NoResults />}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {products.map((item) => (
+                                    <ProductCard
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,7 +9,7 @@ import getProducts from "@/actions/get-products";
 export const revalidate = 0;
 const HomePage = async () => {
     
-    const billboard = await getBillboard("cba71923-1f69-47c4-862f-d4e8f6ef8cf3");
+    const billboard = await getBillboard("9a97b4c1-29b0-4256-933a-023063ea98fc");
     const products = await getProducts({ isFeatured: true});
     
     return(
