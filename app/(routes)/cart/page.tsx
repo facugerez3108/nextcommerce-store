810819e352c19fdfe6b1085a4bde41846ed6c733
unknown <facuugerez37@gmail.com>
2024-03-09 18:53:48 -0300

    fixcart

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -31,7 +31,7 @@ const CartPage = () => {
             <div className="lg:col-span-7">
               {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
               <ul>
-                {cart.items.map((item) => (
+                {cart && cart.items.map((item) => (
                   <CartItem key={item.id} data={item} />
                 ))}
               </ul>
